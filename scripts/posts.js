6feaf9c3b1f6815972e9d6111890270ce7db1d17
Pilotiju <bt1.vietanh@gmail.com>
2026-04-18 18:50:29 +0200

    .

---
+++ scripts/posts.js
@@ -0,0 +1,28 @@
+const posts = [
+    {
+        title: "Title 1",
+        author: "Unknown 1",
+        date: "00.00.0000",
+        content: `
+            lorem
+        `,
+        flair: "meme",
+        postLink: "",
+        upvotesNum: 10,
+        downvotesNum: 10,
+        commentsNum: 10,
+    },
+    {
+        title: "Title 1",
+        author: "Unknown 1",
+        date: "00.00.0000",
+        content: `
+            lorem
+        `,
+        flair: "meme",
+        postLink: "",
+        upvotesNum: 10,
+        downvotesNum: 10,
+        commentsNum: 10,
+    }
+];
