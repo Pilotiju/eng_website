30b0d4a6cdd1c33ebec0f30a92630c10116095fb
Pilotiju <bt1.vietanh@gmail.com>
2026-05-03 16:05:02 +0200

    .

--- scripts/post_actions.js
+++ scripts/post_actions.js
@@ -153,6 +153,8 @@ window.addEventListener('pageshow', () => {
   const postsElHTML = document.getElementsByClassName('js_post_get_data');
   Array.from(postsElHTML).forEach((postEl) => {
     const checkBtnVoted = postEl.querySelector('.js_post__vote_btns_wrapper').classList.contains('post__vote_btn--voted');
+    console.log('postEl:', postEl);
+    console.log('checkBtnVoted:', checkBtnVoted);
     const postElIndex = Number(postEl.getAttribute('data-post-index'));
     if (upvotedPosts.includes(postElIndex) && !checkBtnVoted) {
       toggleUpvotePost(postEl.querySelector('.js_upvote_btn'), '');
