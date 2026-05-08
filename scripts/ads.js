830863395ab48b419fed774f8d24326845a37414
Pilotiju <bt1.vietanh@gmail.com>
2026-05-08 17:06:24 +0200

    working ads in index.html

--- scripts/ads.js
+++ scripts/ads.js
@@ -1,8 +1,33 @@
+let implementedAds = [];
+let ammountOfAdRolls = 2;
+
+function getNotUsedAdIndex() {
+  let adIndex = Math.floor(Math.random() * 3);
+  console.log(`ammountOfAdRolls: ${ammountOfAdRolls}`);
+  ammountOfAdRolls--;
+  if (implementedAds.includes(adIndex)) {
+    return getNotUsedAdIndex();
+  }
+  return adIndex;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const adCollumn = document.querySelector('.js_ads_collumm');
-  adCollumn.innerHTML = /*html*/`
-    <a href="#" target="_blank" class="js_ad_wrapper ad_wrapper">
-      <img src="img/ads/temps/tempad.jpeg" alt="Ad img" class="ad_img">
-    </a>
-  `;
+
+  let html = '';
+  for (let i = 0; i <= 2; i++) {
+    if (ammountOfAdRolls < 0) {
+      break;
+    }
+    const adIndex = getNotUsedAdIndex();
+    implementedAds.push(adIndex);
+    const { img, link } = ads[adIndex];
+    const adHtml = /*html*/`
+      <a href="${link}" target="_blank" class="js_ad_wrapper ad_wrapper">
+        <img src="${img}" alt="Ad img" class="ad_img">
+      </a>
+    `;
+    html += adHtml;
+  }
+  adCollumn.innerHTML = html;
 });
--- scripts/data/data.js
+++ scripts/data/data.js
@@ -1,3 +1,21 @@
+const ads = [
+  // 0
+  {
+    img: "img/ads/nike_air_force.jpeg",
+    link: "https://www.snipes.com/de-de/p/nike-air-force-1-gs-weiss-1740?utm_source=google&utm_source=google&utm_medium=cpc&utm_medium=cpc&utm_campaign=surface&utm_campaign=google_sea_shopping_product_snipes_de_pmax_ftw&gad_source=1&gad_campaignid=22018760131&gbraid=0AAAAADy4gR3UnFK88twsISM95a7kWm2yu&gclid=EAIaIQobChMIl-LijainlAMVGYxQBh0n4TWXEAQYASABEgJREfD_BwE"
+  },
+  // 1
+  {
+    img: "img/ads/joardan_1.jpeg",
+    link: "https://noirfonce.eu/de-de/products/air-jordan-1-retro-high-og-dz5485-106?channable=41a9766964003530373333323335323038353235f4&gad_source=1&gad_campaignid=23190760008&gbraid=0AAAAACvhUjtXNW7-BQWf4uJ7pM3_f9Agy&gclid=EAIaIQobChMIxo245KinlAMVk7CDBx1dyQdjEAQYBCABEgKLbfD_BwE&country=DE"
+  },
+  // 2
+  {
+    img: "img/ads/nike_dunk_low.jpeg",
+    link: "https://www.footlocker.de/de/product/~/316700322304.html?channable=03fd86696400333136373030333232333034303635d6&utm_source=google&utm_medium=cpc&utm_campaign=_1%3Ade_2%3Aperformance_3%3Agoogle_4%3Ado_5%3Apmax-shopping_6%3Aconversion_7%3Amix_8%3Aalways%20on_9%3Amultiple_10%3Ana_16%3Ab4%20-%20best%20sellers&utm_id=18193145981&gad_source=1&gad_campaignid=18188356225&gbraid=0AAAAADpdf7Qa7Jy3k-cLkei2c1VORS-F2&gclid=EAIaIQobChMIhN7poamnlAMVpJCDBx1LiAd5EAQYByABEgJ0NvD_BwE"
+  }
+];
+
 const users = [
   // 0
   {
